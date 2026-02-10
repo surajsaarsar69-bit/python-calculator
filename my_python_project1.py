083f10c9ecf05cd8dfd6fb5b46af42b4b312d66a
surajsaarsar69-bit <surajsaarsar69@gmail.com>
2026-02-10 11:56:29 +0530

    Add files via upload

---
+++ my_python_project1.py
@@ -0,0 +1,19 @@
+# taking two number as input from the user
+#we use float() so the program can handle whole number and decimals
+
+num1=float(input('enter first number: '))
+num2=float(input('enter second number: '))
+
+# performing basic mathematical operations
+
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+
+#displaying the result on the screen
+
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication:",multiplication)
+print("division:",division)
